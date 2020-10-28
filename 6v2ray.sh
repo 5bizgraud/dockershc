7cb2c9f614438003a8aba788d087f6929faf9bfa
5bizgraud <38839867+5bizgraud@users.noreply.github.com>
2020-10-28 18:02:18 +0800

    Create 6v2ray.sh

---
+++ 6v2ray.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+## 用于https://github.com/mixool/dockershc项目安装运行v2ray的脚本
+
+if [[ ! -f "/workerone" ]]; then
+    # install and rename
+    wget -qO- https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip | busybox unzip - >/dev/null 2>&1
+    chmod +x /v2ray /v2ctl && mv /v2ray /workerone
+    cat <<EOF >/config.json
+{
+    "inbounds": 
+    [
+        {
+            "port": "3000","listen": "0.0.0.0","protocol": "vmess",
+            "settings": {"clients": [{"id": "cf43da2c-884c-42c1-ad00-f2f22e98d203"}],"decryption": "none"},
+            "streamSettings": {"network": "ws","wsSettings": {"path": "/ef61fb7c4733f7df0f76ac2ff5183f92/5d24d655/video/m/220f78f17639c464b24900c30f7f77fbb6311620e43c000056cfc17f4827/"}}
+        }
+    ],
+    "outbounds": 
+    [
+        {"protocol": "freedom","tag": "direct","settings": {}},
+        {"protocol": "blackhole","tag": "blocked","settings": {}}
+    ],
+    "routing": 
+    {
+        "rules": 
+        [
+            {"type": "field","outboundTag": "blocked","ip": ["geoip:private"]},
+            {"type": "field","outboundTag": "block","protocol": ["bittorrent"]},
+            {"type": "field","outboundTag": "blocked","domain": ["geosite:category-ads-all"]}
+        ]
+    }
+}
+EOF
+else
+    # start 
+    /workerone -config /config.json >/dev/null 2>&1
+fi
